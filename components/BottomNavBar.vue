9b4ce71681fdc8e55eb4dab751cac1817fef0efb
Gideon Sarfo <aemonsarfo@outlook.com>
2024-03-28 11:49:40 -0400

    UPDATED: Updated BottomNavBar styling and added shadow.
    - Updated the BottomNavBar component in components/BottomNavBar.vue.
    - Added a shadow to the BottomNavBar.
    - Removed the settings icon from the BottomNavBar.
    
    ADDED: Added vertical alignment to search page.
    
    - Updated the search page in pages/search.vue.
    - Added vertical alignment to the flex container.
    - Text now displays in the center of the page.
    - Added a message prompting to log in to make advanced searches.

--- components/BottomNavBar.vue
+++ components/BottomNavBar.vue
@@ -1,10 +1,9 @@
 <template>
   <section class="flex justify-center w-full">
-    <div class="h-[3rem] w-[90vw] bg-[#206A5D] flex items-center justify-evenly rounded-3xl text-[#EBECF1]">
+    <div class="h-[3rem] w-[90vw] bg-[#206A5D] flex items-center justify-evenly rounded-3xl text-[#EBECF1] shadow-lg">
       <NuxtLink to="/home"><Icon name="material-symbols:home" class="w-[2rem] h-[2rem]" :class="{ 'opacity-80': $route.path !== '/home' }" /></NuxtLink>
       <NuxtLink to="/search"><Icon name="material-symbols:search" class="w-[2rem] h-[2rem]" :class="{ 'opacity-80': $route.path !== '/search' }" /></NuxtLink>
       <NuxtLink to="/bookings"><Icon name="ion:ios-calendar" class="w-[2rem] h-[2rem]" :class="{ 'opacity-80': $route.path !== '/bookings' }" /></NuxtLink>
-      <!-- <NuxtLink to="/settings"><Icon name="ion:ios-settings" class="w-[2rem] h-[2rem]" :class="{ 'opacity-80': $route.path !== '/settings' }" /></NuxtLink> -->
     </div>
   </section>
 </template>
--- pages/search.vue
+++ pages/search.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex flex-col items-center gap-5 p-4 min-h-svh">
+  <section class="flex flex-col items-center justify-center gap-5 p-4 min-h-svh">
     <p>log in to make advanced searches</p>
   </section>
 </template>
